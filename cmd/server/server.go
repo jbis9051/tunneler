1711166b994cae727f492dc99a6f6c61a46c0cb1
jbis9051 <josh9051@gmail.com>
2021-03-19 20:42:19 -0400

    Env var

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,10 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 func start(address string) (net.Listener, error) {
-	cer, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
+	certPath, certBool := os.LookupEnv("CERTPATH")
+	keypath, keyBool := os.LookupEnv("KEYPATH")
+	if !certBool || !keyBool {
+		return nil, fmt.Errorf("CERTPATH or KEYPATH unset")
+	}
+	cer, err := tls.LoadX509KeyPair(certPath, keypath)
 	if err != nil {
 		log.Println(err)
 		return nil, err
